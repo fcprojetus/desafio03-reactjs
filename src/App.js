d9937f8f96c2f39af6b0cd0880cb3bfd2f168bc8
Filipe Costa <fcprojetus@gmail.com>
2021-08-25 20:44:37 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repo, setRepo] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(resp => setRepo(resp.data))
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',
+      {
+        title: `Nodejs - ${Date.now()}`,
+        url: "www.github.com",
+        techs: ["Node, Reactjs"]
+      }
+    )
+    setRepo([...repo, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setRepo(()=> repo.filter(item=> item.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repo.length ?
+          repo.map(repo =>
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          ) : null}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
